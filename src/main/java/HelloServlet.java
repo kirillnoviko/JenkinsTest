306fad7bf6884adfc81252883293448779b7d047
Kirill <kiril.novikov.2222@mail.ru>
2022-01-07 02:21:15 +0300

    Initial commit

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,12 @@
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/hello", loadOnStartup = 1)
+public class HelloServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        System.out.println("HelloServlet: You should see this in the Instance Logs :-)");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Boxfuse Sample App: success!</title>
+</head>
+<body style="text-align: center" bgcolor="aqua">
+<textarea>"ddsfsdf"</textarea>
+</body>
+</html>
